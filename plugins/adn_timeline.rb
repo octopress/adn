299c266d52c61d277ae5b704be49c2abadeb7194
Brandon Mathis <brandon@imathis.com>
2013-05-17 16:40:19 -0400

    Update adn_timeline.rb
    Scoped ADN-Timeline under the Octopress module

--- plugins/adn_timeline.rb
+++ plugins/adn_timeline.rb
@@ -2,16 +2,17 @@ unless defined?(Octopress::ConfigTag)
   raise "This plugin requires the config_tag plugin, available at https://github.com/octopress/config-tag"
 end
 
-class ADNTimeline < Liquid::Tag
-  def initialize(tag_name, options, tokens)
-    super
-  end
-
-  def render(context)
-    output = config_tag(context.registers[:site].config, 'adn_timeline')
-    output += "\n<script type='text/javascript'>$(document).ready(function(){ AdnTimeline.init() })</script>"
+module Octopress
+  class ADNTimeline < Liquid::Tag
+    def initialize(tag_name, options, tokens)
+      super
+    end
+  
+    def render(context)
+      output = config_tag(context.registers[:site].config, 'adn_timeline')
+      output += "\n<script type='text/javascript'>$(document).ready(function(){ AdnTimeline.init() })</script>"
+    end
   end
 end
-
-Liquid::Template.register_tag('adn_timeline', ADNTimeline)
+Liquid::Template.register_tag('adn_timeline', Octopress::ADNTimeline)
 

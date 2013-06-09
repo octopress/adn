f55bbc0de31be60db234caf14402c66907286b47
Brandon Mathis <brandon@imathis.com>
2013-06-09 13:36:57 -0700

    updated adn plugin to use the latest Octopress url and config helpers

--- plugins/adn.rb
+++ plugins/adn.rb
@@ -1,43 +1,42 @@
-unless defined?(Octopress::ConfigTag)
-  raise "This plugin requires the config_tag plugin, available at https://github.com/octopress/config-tag"
-end
-
 module Octopress
   module ADN
     class Timeline < Liquid::Tag
+      include ConfigHelper
       def initialize(tag_name, markup, tokens)
         super
       end
     
       def render(context)
-        config = context.registers[:site].config
-        output = config_tag(config, 'adn.timeline')
-        output += "\n<script type='text/javascript'>$(document).ready(function(){ timeline = require('adn/adn-timeline'); new timeline({username: '#{config['adn.username']}'}) })</script>"
+        output = config_tag(context, 'adn.timeline')
+        output += "\n<script type='text/javascript'>$(document).ready(function(){ timeline = require('adn/adn-timeline'); new timeline({username: '#{config(context, 'adn.username')}'}) })</script>"
       end
     end
     class Share < Liquid::Tag
+      include ConfigHelper
       def initialize(tag_name, markup, tokens)
         super
       end
     
       def render(context)
-        config = context.registers[:site].config
+        username = config(context, 'adn.username')
         <<-tag
-          <a href="https://alpha.app.net/intent/follow/?user_id=%#{config['adn']['username']}" class='adn-button' target='_blank' data-type='follow' data-width='277' data-height='27' data-user-id='@imathis' data-show-username='1' rel='me'>Follow me on App.net</a>
+          <a href="https://alpha.app.net/intent/follow/?user_id=%40#{username}" class='adn-button' target='_blank' data-type='follow' data-width='277' data-height='27' data-user-id='@#{username}' data-show-username='1' rel='me'>Follow me on App.net</a>
           <script>(function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src='//d2zh9g63fcvyrq.cloudfront.net/adn.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'adn-button-js'));</script>
         tag
       end
     end
     class Follow < Liquid::Tag
+      include UrlHelpers
       def initialize(tag_name, markup, tokens)
         @markup = markup
         super
       end
     
       def render(context)
+        url = encode_url page_url(context)
         <<-tag
-<a href='https://alpha.app.net/intent/post/?url=http%3A%2F%2Foctopress.org' class='adn-button' target='_blank' data-type='share' data-width='132' data-height='27' data-text='' data-url='http:&#x2F;&#x2F;octopress.org' >Share on App.net</a>
-<script>(function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src='//d2zh9g63fcvyrq.cloudfront.net/adn.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'adn-button-js'));</script>
+          <a href='https://alpha.app.net/intent/post/?url=#{url}' class='adn-button' target='_blank' data-type='share' data-width='132' data-height='27' data-text='' data-url='#{url}' >Share on App.net</a>
+          <script>(function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src='//d2zh9g63fcvyrq.cloudfront.net/adn.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'adn-button-js'));</script>
         tag
       end
     end
@@ -47,4 +46,4 @@ end
 Liquid::Template.register_tag('adn_timeline', Octopress::ADN::Timeline)
 Liquid::Template.register_tag('adn_share', Octopress::ADN::Share)
 Liquid::Template.register_tag('adn_follow', Octopress::ADN::Follow)
-
+ 
